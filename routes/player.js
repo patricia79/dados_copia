d8a15768a82a8f6238694e009e7feccbdcc4ae7e
Patricia <waltraute@hotmail.com>
2022-05-28 22:16:58 +0200

    afegir rutes de addplayergame

--- routes/player.js
+++ routes/player.js
@@ -1,8 +1,10 @@
 const express = require("express");
 const api = express.Router();
 
-const { addNewPlayer} = require('../controllers/player');
+const { addNewPlayer, getAllPlayers, addPlayerGame} = require('../controllers/player');
 
 api.post('/players', addNewPlayer) 
+api.get('/players', getAllPlayers) 
+api.post('/players/:id/games', addPlayerGame)
 
 module.exports = api
