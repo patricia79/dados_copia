2729e9fcf49312445aeb1c867b7f46a9bdc7899e
Patricia <waltraute@hotmail.com>
2022-05-28 22:17:29 +0200

    afegir addplayergame

--- data/mySQLCRUD.js
+++ data/mySQLCRUD.js
@@ -4,5 +4,17 @@ async function  addPlayer (player) {
 
      return await Player.create({name: player.name, registerDate: player.register_date});
  }
+
+ async function  getAllPlayers () {
+
+    return await Player.findAll();
+}
+
+async function getPlayer(id) {
+    return await Player.findOne({where: {idPlayer: id}});
+}
   
-module.exports = { addPlayer };
+module.exports = { addPlayer, getAllPlayers, getPlayer };
+
+
+   
