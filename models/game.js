2568352302d52a028af273f00d4aa1e192ef24d9
Patricia <waltraute@hotmail.com>
2022-05-29 10:34:48 +0200

    modificar idGame

--- models/game.js
+++ models/game.js
@@ -3,7 +3,7 @@
 class Game {
 
     constructor(dice1, dice2, score, result, idPlayer) {
-        this.id = null
+        this.idGame = null
         this.dice1 = dice1
         this.dice2 = dice2 
         this.score =  score
