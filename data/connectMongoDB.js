be7c941dcc86dac10e813bba7c82914d0ff989b5
Patricia <waltraute@hotmail.com>
2022-05-29 10:33:01 +0200

    afegir col game i player

--- data/connectMongoDB.js
+++ data/connectMongoDB.js
@@ -17,3 +17,33 @@ throw err;
 }
 });
 
+'use strict'
+
+const mongoose = require("mongoose");
+const Schema  = mongoose.Schema;
+
+let GameSchema = Schema({
+    idGame: Number,
+    dice1 : Number,
+    dice2 : Number,
+    result: Number,
+    score: Boolean,
+    idPlayer: {type: Schema.ObjectId, ref: "Player"} //Referenciara un id d'objecte de la col.leccio Player
+});
+
+module.exports = mongoose.model("Game",GameSchema);
+
+
+'use strict' //per emprar expresions de js més modern
+
+const mongoose = require("mongoose")
+const Schema = mongoose.Schema; //Ens permetra el crear un objecte tipus schema de la bd i que posteriorment al guardar dades s'enmagatzamaran dins d'una coleccio concreta i alhora en un document.
+
+let PlayerSchema = Schema({
+    namePlayer: String,
+    idPlayer: Number,
+    registerDate: Date,
+    
+});
+
+module.exports = mongoose.model("Player", PlayerSchema); //Exportem el model i genera un objecte anomenat Player, realitzant una instancia de totes les dades que nosaltres haguem enmagatzemat amb l'esquema definit
