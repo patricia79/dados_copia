742ac1938a9e9fb8677ff378daadea7e60a9e2f0
Patricia <waltraute@hotmail.com>
2022-06-02 13:56:43 +0200

    actualizar rutas

--- routes/player.js
+++ routes/player.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const api = express.Router();
 
-const { addNewPlayer, getAllPlayers, addPlayerGame, getAllGames, ranking, deletePlayerGames, modifyPlayer} = require('../controllers/player');
+const { addNewPlayer, getAllPlayers, addPlayerGame, getAllGames, ranking, modifyPlayer, deletePlayerGames} = require('../controllers/player');
 
 api.post('/players', addNewPlayer)  // POST /players: crea un jugador
 api.get('/players', getAllPlayers) // GET /players: retorna el llistat de tots els jugadors del sistema amb el seu percentatge mig d’èxits 
@@ -10,8 +10,8 @@ api.get('/games/id:', getAllGames) // GET /games/{id}: retorna el llistat de jug
 api.get('/ranking', ranking) // GET /ranking: retorna un ranking de jugadors ordenat per percentatge d'èxits i el percentatge d’èxits mig del conjunt de tots els jugadors
 api.put('players/id:', modifyPlayer) // PUT players/{id}: modifica el nom del jugador
 api.delete('/games/id:', deletePlayerGames) // DELETE /games/{id}: elimina les tirades del jugador
-// api.get('/players/ranking/loser', loserPlayer) // GET /players/ranking/loser: retorna el jugador amb pitjor percentatge d’èxit
-// api.get('/players/ranking/winner', winnerPlayer ) // GET /players/ranking/winner: retorna el jugador amb millor percentatge d’èxit
+// api.get('/ranking/loser', loserPlayer) // GET /players/ranking/loser: retorna el jugador amb pitjor percentatge d’èxit
+// api.get('/ranking/winner', winnerPlayer ) // GET /players/ranking/winner: retorna el jugador amb millor percentatge d’èxit
 
 
 module.exports = api
